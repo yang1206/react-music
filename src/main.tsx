d40832e45569e8a79b69c4f285175aa466ca041a
yang1206 <yang15188280673@outlook.com>
2022-07-29 15:45:45 +0800

    style: 🎨 layout自适应

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '@/assets/css/reset.less'
+import '@/assets/css/reset.css'
+import 'normalize.css/normalize.css'
+import 'antd/dist/antd.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
