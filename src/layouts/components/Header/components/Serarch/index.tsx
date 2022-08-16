db155052cd58a85c088945cb071e5c3525a76c5d
yang1206 <yang15188280673@outlook.com>
2022-08-16 20:36:01 +0800

    fix: 🧩 fix

--- src/layouts/components/Header/components/Serarch/index.tsx
+++ src/layouts/components/Header/components/Serarch/index.tsx
@@ -16,13 +16,15 @@ const Search: React.FC = () => {
   const focusState = useAppSelector(selectFocusState).data
   const dispatch = useAppDispatch()
   const changeInput = debounce((target: { value: string }) => {
-    let value = target.value.trim()
+    let targetValue = target.value.trim()
+    setValue(targetValue)
     if (value.length < 1) return
     // 显示下拉框
     dispatch(changeFocusState(true))
     // 发送网络请求
     dispatch(getSearchSongList(value))
-  }, 400)
+  }, 100)
+
   // 表单回车:跳转到搜索详情
   const handleEnter = useCallback(() => {
     // 说明当前光标有”高亮当前行“
@@ -36,11 +38,8 @@ const Search: React.FC = () => {
   }, [dispatch, recordActive, searchSongList])
   // 获取焦点
   const handleFocus = useCallback(() => {
-    // 当文本获取焦点时,文本被选中状态
-    // inputRef.current.select()
     // 更改为获取焦点状态
     dispatch(changeFocusState(true))
-    // 修改状态重定向状态
   }, [dispatch])
   //失去搜索框焦点事件
   const handleBlur = useCallback(() => {
@@ -77,35 +76,14 @@ const Search: React.FC = () => {
     // 播放音乐
     // document.getElementById('audio').autoplay = true
   }
+  //路由跳转并携带参数
   const redirect = () => {
-    if (value !== '') {
-      navigate('/search/single', {
-        replace: false,
-        state: {
-          song: value,
-          type: 1
-        }
+    if (value.length > 0) {
+      navigate(`/search/single?song=${value}&type=1`, {
+        replace: false
       })
     }
   }
-  // icons键盘图标
-  const icons = (
-    <div className="icons-wrapper">
-      <div className="ctrl-wrapper">
-        <svg width="15" height="15" className="DocSearch-Control-Key-Icon">
-          <path
-            d="M4.505 4.496h2M5.505 5.496v5M8.216 4.496l.055 5.993M10 7.5c.333.333.5.667.5 1v2M12.326 4.5v5.996M8.384 4.496c1.674 0 2.116 0 2.116 1.5s-.442 1.5-2.116 1.5M3.205 9.303c-.09.448-.277 1.21-1.241 1.203C1 10.5.5 9.513.5 8V7c0-1.57.5-2.5 1.464-2.494.964.006 1.134.598 1.24 1.342M12.553 10.5h1.953"
-            strokeWidth="1.2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="square"
-          ></path>
-        </svg>
-      </div>
-      <div className="k-wrapper">k</div>
-    </div>
-  )
-
   return (
     <div className="search-Wrapper">
       <Input
@@ -120,7 +98,6 @@ const Search: React.FC = () => {
         onPressEnter={() => handleEnter()}
         value={value}
         onKeyDown={watchKeyboard}
-        suffix={icons}
         onBlur={handleBlur}
       />
       <div className="down-slider" style={{ display: focusState ? 'block' : 'none' }}>
