20469e2e8798e094a4b258c654e14e5b2e2fddc4
yang1206 <yang15188280673@outlook.com>
2022-08-18 17:43:49 +0800

    fix: 🧩 修复bug

--- src/views/search/index.tsx
+++ src/views/search/index.tsx
@@ -1,5 +1,8 @@
 import { memo } from 'react'
+import { useSearchParams } from 'react-router-dom'
 const Search: React.FC = () => {
-  return <div>搜索</div>
+  const [params] = useSearchParams()
+  const song = params.get('song')
+  return <div>你搜索的{song}</div>
 }
 export default memo(Search)
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   server: {
     host: true, // host设置为true才可以使用network的形式，以ip访问项目
-    port: 3000, // 端口号
+    port: 1206, // 端口号
     open: true, // 自动打开浏览器
     cors: true, // 跨域设置允许
     strictPort: true // 如果端口已占用直接退出
