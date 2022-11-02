2ad856f1d1e12548593ce4f6d8a7dea73613cc0b
yang1206 <yang15188280673@outlook.com>
2022-11-02 08:02:33 +0800

    fix: 🧩 build

--- netlify/edge-functions/proxy.ts
+++
@@ -1,5 +0,0 @@
-import type { Context } from 'https://edge.netlify.com'
-
-export default async (request: Request, context: Context) => {
-  return context.rewrite('https://netease-cloud-music-api-yang1206.vercel.app/')
-}
